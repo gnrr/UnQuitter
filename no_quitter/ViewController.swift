1258b0cc70c32af259408295db9722081bf62965
gnrr <hideaki.sh@gmail.com>
2017-12-30 09:51:26 +0900

    add test code for foreground

--- no_quitter/ViewController.swift
+++ no_quitter/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoadが呼ばれたぞ")
+        
+        // フォアグラウンドになったときに呼ばれるメソッドを登録
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.myEnterForeground),
+            name: NSNotification.Name.UIApplicationWillEnterForeground,
+            object: nil
+        )
+    }
+
+    @objc func myEnterForeground(_ notification: Notification?) {
+        if (self.isViewLoaded && (self.view.window != nil)) {
+            print("フォアグラウンドになったぞ")
+        }
     }
 
     override func didReceiveMemoryWarning() {
